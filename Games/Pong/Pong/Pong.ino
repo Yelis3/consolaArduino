4d09affd03caf4bda13366a258cc5dd9e6b654eb
Ruben Buelvas <rcbuelvasv@gmail.com>
2017-09-14 21:57:55 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ Games/Pong/Pong/Pong.ino
@@ -0,0 +1,55 @@
+//Vamos a hacer un pong :D
+
+//Variables del manejo de la matriz
+#include <LedControl.h>
+int timer = 10;
+int pinDIN = 12;
+int pinCS = 7;
+int pinCLK = 8; 
+
+//Coordenadas iniciales
+int ballX = 4, ballY = 4;
+int p1X = 0, p1Y = 3;
+int p2X = 7, p2Y = 3;
+
+//X y Y son las coordenadas actuales
+//dirX: -1 izquierda, 1 derecha
+//dirY -1 arriba, 0 centro, 1 abajo
+void drawBall(int x, int y, int dirX, int dirY)
+{
+  lc.setLed(0, x, y, false);
+  lc.setLed(0, x+dirX, y+dirY);
+}
+
+
+void drawPlayer(int id, int y, int dirY)
+{
+  
+  if(id == 1)
+  {
+    lc.setLed(0 , 0, y, false);
+    lc.setLed(0 , 0, y+1, false);
+    if((dirY == 1 && y < 6) || (dirY == -1 && y > 0))
+    {
+      lc.setLed(0, 0, y+dirY, true);
+      lc.setLed(0, 0, y+dirY+1, true);
+    }
+  }
+  else if(id == 2)
+  {
+    
+  }
+}
+
+void setup() 
+{
+  pinMode(13,OUTPUT);
+  lc.shutdown(0,false);
+  lc.setIntensity(0,8);
+}
+
+void loop() 
+{
+  
+
+}
